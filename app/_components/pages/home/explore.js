6b8d4ba4ca489e2c127556014760f62e23dea6ea
abhishekvithani2811 <abhishekvithani2811@gmail.com>
2024-09-17 01:39:36 +0530

    108

--- app/_components/pages/home/explore.js
+++ app/_components/pages/home/explore.js
@@ -1,11 +1,18 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
 import SectionHeader from '../../layout/section-header';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const categories = [
     { name: 'Rings', image: '/assets/explore/image.png' }, // Replace with your image URL
     { name: 'Earrings', image: '/assets/explore/image-1.png' }, // Replace with your image URL
     { name: 'Necklaces', image: '/assets/explore/image-2.png' }, // Replace with your image URL
+    { name: 'Bracelets', image: '/assets/explore/image-3.png' }, // Replace with your image URL
+    { name: 'Bracelets', image: '/assets/explore/image-3.png' }, // Replace with your image URL
+    { name: 'Bracelets', image: '/assets/explore/image-3.png' }, // Replace with your image URL
     { name: 'Bracelets', image: '/assets/explore/image-3.png' }, // Replace with your image URL
 ];
 
@@ -16,19 +23,40 @@ export default function ExploreSection() {
                 title="Explore The Range"
                 subtitle="SHOP BY CATEGORY"
             />
-            <div className="mt-12 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-                {categories.map((category) => (
-                    <Link href="/products" key={category.name} className="group relative cursor-pointer">
-                        <div className="w-full md:h-80 1xl:h-96 h-72 bg-gray-200 overflow-hidden group-hover:opacity-75">
-                            <img
-                                src={category.image}
-                                alt={category.name}
-                                className="w-full h-full object-center object-cover"
-                            />
-                        </div>
-                        <h3 className="mt-1 text-xl font-semibold text-gray-900" style={{ fontFamily: 'Futura PT', fontWeight: '400', lineHeight: '36px' }}>{category.name}</h3>
-                    </Link  >
-                ))}
+            <div className="mt-12">
+                <Swiper 
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1.5,
+                        },
+                        640: {
+                            slidesPerView: 2.5,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                        },
+                    }}
+                    loop
+                    className="mySwiper"
+                >
+                    {categories.map((category) => (
+                        <SwiperSlide  className="group relative cursor-pointer">
+
+                            <Link href="/products" key={category.name} className="group relative cursor-pointer">
+                                <div className="w-full md:h-80 1xl:h-96 md:h-72 bg-gray-200 overflow-hidden group-hover:opacity-75">
+                                    <img
+                                        src={category.image}
+                                        alt={category.name}
+                                        className="md:w-full md:h-full object-center object-cover"
+                                    />
+                                </div>
+                                <h3 className="mt-1 text-xl font-semibold text-gray-900" style={{ fontFamily: 'Futura PT', fontWeight: '400', lineHeight: '36px' }}>{category.name}</h3>
+                            </Link  >
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
             {/* <div className="mt-8 text-center">
                 <button className="px-6 py-2 text-black border border-black hover:bg-gray-200">View More</button>
--- app/_components/pages/products/selectedGallary.js
+++ app/_components/pages/products/selectedGallary.js
@@ -1,52 +1,61 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
 import { FaCheck } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export default function SelectedGallary() {
     const categories = [
         { name: 'Statement', image: '/assets/products/1.png' },
         { name: 'Engagement', image: '/assets/products/2.png' },
+        { name: 'Statement', image: '/assets/products/1.png' },
+        { name: 'Eternity', image: '/assets/products/3.png' },
         { name: 'Eternity', image: '/assets/products/3.png' },
         { name: 'Stacking', image: '/assets/products/4.png' },
     ];
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true
-    };
 
     return (
         <>
-            <div className="py-8 px-4 sm:px-6 lg:px-8 bg-white">
-
-                <div className="mt-12 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-                    
-                    {categories.map((category, index) => (
-                        <div key={category.name} className="group relative cursor-pointer">
-                            <div className="w-full  bg-gray-200 overflow-hidden group-hover:opacity-75 relative">
-
-                                <div className="w-full h-full bg-gray-200 overflow-hidden group-hover:opacity-75">
-                                    <img
-                                        src={category.image}
-                                        alt={category.name}
-                                        className="w-full h-full object-center object-cover"
-                                    />
-                                </div>
+            <div className="py-2 px-4 sm:px-6 lg:px-8 bg-white">
+                <Swiper
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1.5,
+                        },
+                        640: {
+                            slidesPerView: 2.5,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                        },
+                    }}
+                    loop
+                    className="mySwiper"
+                >
+                    {categories?.map((category, index) => (
+                        <SwiperSlide key={index} className="group relative cursor-pointer">
+                            <div className="w-full bg-gray-200 overflow-hidden group-hover:opacity-75 relative">
+                                <Image
+                                    src={category.image}
+                                    alt={category.name}
+                                    width={400}
+                                    height={400}
+                                    className="w-full h-full object-center object-cover"
+                                />
                                 {index === 0 && (
                                     <div className="absolute top-0 right-0 p-2">
                                         <Image src='/assets/products/5.png' width={30} height={30} className='h-full w-full' />
                                     </div>
                                 )}
                             </div>
-
                             <h3 className="mt-1 text-xl font-semibold text-gray-900" style={{ fontFamily: 'Futura PT', fontWeight: '400', lineHeight: '36px', textDecoration: index === 0 ? 'underline' : 'none' }}>{category.name}</h3>
-                        </div>
+                        </SwiperSlide>
                     ))}
-                </div>
-
+                </Swiper>
             </div>
         </>
     )
